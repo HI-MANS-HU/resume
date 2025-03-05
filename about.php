10542261c07b887388bc39a23f46af48ee86cd64
HI-MANS-HU <himanshusahu408@gmail.com>
2025-03-06 01:02:05 +0530

    added

---
+++ about.php
@@ -0,0 +1,101 @@
+<?php require_once './header.php'; ?>
+<h1 class="head1">About Us</h1>
+<hr>
+
+<div class="text-center bg-dark text-white pt-2 rounded">
+      <div class="carousel slide" data-ride="carousel" id="car1">
+            <div class="carousel-inner">
+                <div class="carousel-item active" >
+                    <p><strong>Empowering people to believe.</strong></p>
+                </div>
+                <div class="carousel-item">
+                    <p>The Student Tutoring Network</p>
+                </div>
+                <div class="carousel-item">
+                    <p>The trusted name for in-home tutoring</p>
+                </div>
+            </div>
+              <a href="#car1" data-slide="prev" class="carousel-control-prev">
+                <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a href="#car1" data-slide="next" class="carousel-control-next">
+                  <span class="carousel-control-next-icon"></span>
+                </a>
+
+              </div>        
+</div>
+
+<div class="row my-5 text-justify">
+    <div class="col-sm-5">        
+        <p>
+Tutoring is a great way for students to get their questions answered and improve their grades. Tutors work at your pace and tailor their teaching to meet your learning needs.            
+        </p>
+        <p>
+Tutor Finder can put you in touch with English Language Tutors, Maths Tutors, Science Tutors, Music Teachers, Foreign Language Tutors, Social Science Tutors, Health & Medical Tutors and Business Specialists.            
+        </p>
+    </div>
+    <div class="col-sm-7">        
+          <div class="col-sm-12">
+    <div class="carousel slide" data-ride="carousel" id="car2">
+      <ul class="carousel-indicators">
+          <li data-target="#car2" data-slide-to="0" class="active"></li>
+          <li data-target="#car2" data-slide-to="1"></li>
+          <li data-target="#car2" data-slide-to="2"></li>
+          <li data-target="#car2" data-slide-to="3"></li>
+          <li data-target="#car2" data-slide-to="4"></li>
+        </ul>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="img-fluid"  src="images/c1.jpg"/>
+                <div  class="carousel-caption">
+                  <h1>Marin Drive : Raipur</h1>
+                </div>
+              </div>
+              <div class="carousel-item">
+                  <img class="img-fluid"  src="images/c2.jpg"/>
+                  <div class="carousel-caption">
+                      <h1>Bastar</h1>
+                    </div>
+                    </div>
+                <div class="carousel-item">
+                    <img class="img-fluid"  src="images/c3.jpg"/>
+                    <div class="carousel-caption">
+                      <h1>Bla bla</h1>
+                      </div>
+                    </div>
+            
+                            <div class="carousel-item">
+                    <img class="img-fluid"  src="images/c4.jpg"/>
+                    <div class="carousel-caption">
+                      <h1>Jungle Safari</h1>
+                      </div>
+                    </div>
+
+                            <div class="carousel-item">
+                    <img class="img-fluid"  src="images/c5.jpg"/>
+                    <div class="carousel-caption">
+                      <h1>Ambikapur</h1>
+                      </div>
+                    </div>
+
+             </div>
+             <a href="#car2" data-slide="prev" class="carousel-control-prev">
+                <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a href="#car2" data-slide="next" class="carousel-control-next">
+                  <span class="carousel-control-next-icon"></span>
+                </a>
+        </div>    
+  </div>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12">
+                <p>According to our compiled statistics and research, we noticed that students learn better at their convenience, because more attention is paid to their needs when compared to classrooms, where the teacher-student proportion is alarming. To make learning easy, we decided to bridge the gap and reduce the ratio.</p>        
+                <p>At Tutor Finder, our emphasis is on the quality of learning given out by our tutors. That is why we are specific about the learners’ interest so as to enable us match it to the expertise of the tutors. We can make boast of about 54 experienced, well-qualified tutors, who have passed through effective training and tests - testing their intelligence and skill, and preparing them for easy adaptation to student specifications and behaviors.
+</p>
+    </div>
+</div>
+<?php require_once './footer.php'; ?>
+
+
